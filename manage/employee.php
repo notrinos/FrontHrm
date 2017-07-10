4d2befa439783c5d4bb093f94c5dfc355a8121a0
Anh Phương <trananhphuong83@gmail.com>
2017-07-10 09:47:06 +0700

    Create employee.php

---
+++ manage/employee.php
@@ -0,0 +1,37 @@
+<?php
+/*=======================================================\
+|                        FrontHrm                        |
+|--------------------------------------------------------|
+|   Creator: Phương                                      |
+|   Date :   09-07-2017                                  |
+|   Description: Frontaccounting Payroll & Hrm Module    |
+|   Free software under GNU GPL                          |
+|                                                        |
+\=======================================================*/
+
+$page_security = 'SA_EMPL';
+$path_to_root  = '../../..';
+
+include_once($path_to_root . "/includes/session.inc");
+add_access_extensions();
+
+$js = "";
+if ($SysPrefs->use_popup_windows)
+	$js .= get_js_open_window(900, 500);
+if (user_use_date_picker())
+	$js .= get_js_date_picker();
+
+include_once($path_to_root . "/includes/ui.inc");
+include_once($path_to_root . "/modules/FrontHrm/includes/frontHrm_db.inc");
+include_once($path_to_root . "/modules/FrontHrm/includes/frontHrm_ui.inc");
+
+//--------------------------------------------------------------------------
+
+page(_($help_context = "Manage Employees"), '', false, "", $js);
+
+start_form(true);
+
+
+
+end_form();
+end_page();
