a783b476c74e9c072ede4990bee6fb372d25f9f5
Anh Phương <trananhphuong83@gmail.com>
2017-07-18 16:14:04 +0700

    Update remove.sql

--- sql/remove.sql
+++ sql/remove.sql
@@ -3,3 +3,5 @@ DROP TABLE IF EXISTS `0_department`;
 DROP TABLE IF EXISTS `0_salaryscale`;
 DROP TABLE IF EXISTS `0_overtime`;
 DROP TABLE IF EXISTS `0_attendance`;
+DROP TABLE IF EXISTS `0_payroll_account`;
+DROP TABLE IF EXISTS `0_payroll_structure`;
