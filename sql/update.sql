e82237ad84a40565405e4babdbf18810fc0026dd
Anh Phương <trananhphuong83@gmail.com>
2017-09-18 08:25:58 +0700

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `0_employee` (
     `emp_id` int(11) NOT NULL AUTO_INCREMENT,
     `emp_first_name` varchar(100) DEFAULT NULL,
     `emp_last_name` varchar(100) DEFAULT NULL,
-	`gender` tinyint(1) NOT NULL DEFAULT '0',
+	`gender` tinyint(1) NOT NULL DEFAULT 0,
     `emp_address` tinytext,
     `emp_mobile` varchar(30) DEFAULT NULL,
     `emp_email` varchar(100) DEFAULT NULL,
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS `0_employee` (
     `emp_notes` tinytext NOT NULL,
     `emp_hiredate` date DEFAULT NULL,
     `department_id` int(11) NOT NULL,
-    `salary_scale_id` int(11) NOT NULL,
+    `salary_scale_id` int(11) NOT NULL DEFAULT 0,
     `emp_releasedate` date DEFAULT NULL,
-    `inactive` tinyint(1) NOT NULL DEFAULT '0',
+    `inactive` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`emp_id`),
     KEY `salary_scale_id` (`salary_scale_id`),
     KEY `department_id` (`department_id`)
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `0_department`;
 CREATE TABLE IF NOT EXISTS `0_department` (
     `dept_id` int(11) NOT NULL AUTO_INCREMENT,
     `dept_name` tinytext NOT NULL,
-    `inactive` tinyint(1) NOT NULL DEFAULT '0',
+    `inactive` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`dept_id`)
 ) ENGINE=InnoDB;
 
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS `0_salaryscale`;
 CREATE TABLE IF NOT EXISTS `0_salaryscale` (
     `scale_id` int(11) NOT NULL AUTO_INCREMENT,
     `scale_name` text NOT NULL,
-    `inactive` tinyint(1) NOT NULL DEFAULT '0',
+    `inactive` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`scale_id`)
 ) ENGINE=InnoDB;
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `0_overtime` (
     `overtime_id` int(11) NOT NULL AUTO_INCREMENT,
 	  `overtime_name` varchar(100) NOT NULL,
     `overtime_rate` float(5) NOT NULL,
-    `inactive` tinyint(1) NOT NULL DEFAULT '0',
+    `inactive` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`overtime_id`)
 ) ENGINE=InnoDB;
 
@@ -52,7 +52,7 @@ DROP TABLE IF EXISTS `0_attendance`;
 CREATE TABLE IF NOT EXISTS `0_attendance` (
     `emp_id` int(11) NOT NULL,
     `overtime_id` int(11) NOT NULL,
-    `hours_no` int(11) NOT NULL DEFAULT '0',
+    `hours_no` int(11) NOT NULL DEFAULT 0,
     `att_date` date NOT NULL,
     PRIMARY KEY (`emp_id`,`overtime_id`,`att_date`)
 ) ENGINE=InnoDB;
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `0_salary_structure` (
   `salary_scale_id` int(11) NOT NULL,
   `pay_rule_id` int(11) NOT NULL,
   `pay_amount` double NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '0 for credit,1 for debit',
+  `type` tinyint(1) NOT NULL COMMENT '0 for credit, 1 for debit',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
@@ -99,4 +99,5 @@ CREATE TABLE IF NOT EXISTS `0_payslip_detail` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-ALTER TABLE `0_gl_trans` ADD `payslip_no` INT(11) NOT NULL DEFAULT '0';
+
+ALTER TABLE `0_gl_trans` ADD `payslip_no` INT(11) NOT NULL DEFAULT 0;
