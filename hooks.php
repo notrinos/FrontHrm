248e185e33e028cb3740e9dbccabe48dd4a6b70c
Anh Phương <trananhphuong83@gmail.com>
2017-09-19 09:01:28 +0700

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -18,20 +18,20 @@ class FrontHrm_app extends application {
         $this->application("FrontHrm", _($this->help_context = "Human Resource"));
         
         $this->add_module(_("Transactions"));
-		$this->add_lapp_function(0, 'Attendance', $path_to_root.'/modules/FrontHrm/manage/manage_attendance.php', 'SA_EMPL', MENU_TRANSACTION);
+		$this->add_lapp_function(0, 'Attendance', $path_to_root.'/modules/FrontHrm/manage/attendance.php', 'SA_EMPL', MENU_TRANSACTION);
         $this->add_lapp_function(0, 'Payslip Entry', $path_to_root.'/modules/FrontHrm/manage/payslip.php?NewPayslip=Yes', 'SA_EMPL', MENU_TRANSACTION);
    
         $this->add_module(_("Inquiries and Reports"));
 		$this->add_lapp_function(1, 'Timesheet', $path_to_root.'/modules/FrontHrm/inquiry/time_sheet.php', 'SA_EMPL', MENU_INQUIRY);
         
         $this->add_module(_("Maintenance"));
-		$this->add_lapp_function(2, 'Employees', $path_to_root.'/modules/FrontHrm/manage/manage_employee.php', 'SA_EMPL', MENU_ENTRY);
+		$this->add_lapp_function(2, 'Employees', $path_to_root.'/modules/FrontHrm/manage/employee.php', 'SA_EMPL', MENU_ENTRY);
 		$this->add_lapp_function(2, 'Departments', $path_to_root.'/modules/FrontHrm/manage/department.php', 'SA_HRSETUP', MENU_MAINTENANCE);
-		$this->add_lapp_function(2, 'Manage Overtime', $path_to_root.'/modules/FrontHrm/manage/manage_overtime.php', 'SA_HRSETUP', MENU_MAINTENANCE);
+		$this->add_lapp_function(2, 'Manage Overtime', $path_to_root.'/modules/FrontHrm/manage/overtime.php', 'SA_HRSETUP', MENU_MAINTENANCE);
 		
-        $this->add_rapp_function(2, 'Salary Scales', $path_to_root.'/modules/FrontHrm/manage/manage_salaryscale.php', 'SA_HRSETUP', MENU_MAINTENANCE);
-		$this->add_rapp_function(2, 'Payroll Accounts', $path_to_root.'/modules/FrontHrm/manage/manage_accounts.php', 'SA_HRSETUP', MENU_MAINTENANCE);
-		$this->add_rapp_function(2, 'Payroll Rules', $path_to_root.'/modules/FrontHrm/manage/manage_payroll_rules.php', 'SA_HRSETUP', MENU_MAINTENANCE);
+        $this->add_rapp_function(2, 'Salary Scales', $path_to_root.'/modules/FrontHrm/manage/salaryscale.php', 'SA_HRSETUP', MENU_MAINTENANCE);
+		$this->add_rapp_function(2, 'Payroll Accounts', $path_to_root.'/modules/FrontHrm/manage/accounts.php', 'SA_HRSETUP', MENU_MAINTENANCE);
+		$this->add_rapp_function(2, 'Payroll Rules', $path_to_root.'/modules/FrontHrm/manage/payroll_rules.php', 'SA_HRSETUP', MENU_MAINTENANCE);
         $this->add_rapp_function(2, 'Salary Structure', $path_to_root.'/modules/FrontHrm/manage/salary_structure.php', 'SA_HRSETUP', MENU_MAINTENANCE);
 		$this->add_extensions();
     }
