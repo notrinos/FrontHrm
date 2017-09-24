06f2e1de7ea34dd6235333e55816ab65dbe4ccd2
Anh Phương <trananhphuong83@gmail.com>
2017-09-24 13:09:47 +0700

    Update attendance.php

--- manage/attendance.php
+++ manage/attendance.php
@@ -28,11 +28,23 @@ include_once($path_to_root . "/modules/FrontHrm/includes/frontHrm_ui.inc");
 //--------------------------------------------------------------------------
 
 function can_process() {
+
+	if (!is_date($_POST['AttDate'])) {
+
+		display_error(_("The entered date is invalid."));
+		set_focus('AttDate');
+		return false;
+	}
+	if (date_comp($_POST['AttDate'], Today()) > 0) {
+
+		display_error(_("Cannot make attendance for the date in the future."));
+		set_focus('AttDate');
+		return false;
+	} 
 	
 	foreach(db_query(get_employees(false, false, get_post('DeptId'))) as $emp) {
 		
-		if(strlen($_POST[$emp['emp_id'].'-0']) != 0 && !is_numeric($_POST[$emp['emp_id'].'-0']))
-		{
+		if(strlen($_POST[$emp['emp_id'].'-0']) != 0 && !is_numeric($_POST[$emp['emp_id'].'-0'])) {
 			display_error(_("Overtime hours must be a number."));
 			set_focus($emp['emp_id'].'-0');
 			return false;
@@ -58,7 +70,7 @@ start_form();
 
 start_table(TABLESTYLE_NOBORDER);
 start_row();
-date_cells(_("Date:"), 'attend_date');
+date_cells(_("Date:"), 'AttDate');
 department_list_cells(_("For department:"), "DeptId", null, _("All departments"), true);
 end_row();
 end_table(1);
@@ -119,7 +131,7 @@ if(isset($_POST['addatt'])) {
     $att_items = 0;
     foreach($emp_ids as $id) {
         
-		if($_POST[$id.'-0'] && check_date_paid($id, $_POST['attend_date'])) {
+		if($_POST[$id.'-0'] && check_date_paid($id, $_POST['AttDate'])) {
 			
 			display_error("Selected date has already paid for Employee $id");
             set_focus($id.'-0');
@@ -127,12 +139,12 @@ if(isset($_POST['addatt'])) {
 		}
 		else {
 			$att_items += $_POST[$id.'-0'];
-			write_attendance($id, 0, $_POST[$id.'-0'], $_POST['attend_date']);
+			write_attendance($id, 0, $_POST[$id.'-0'], $_POST['AttDate']);
 		}
         
         foreach($overtime_id as $ot) {
 			
-			if($_POST[$id.'-0'] && check_date_paid($id, $_POST['attend_date'])){
+			if($_POST[$id.'-0'] && check_date_paid($id, $_POST['AttDate'])){
 			
 				display_error("Selected date has already paid for Employee $id");
             	set_focus($id.'-'.$ot);
@@ -140,7 +152,7 @@ if(isset($_POST['addatt'])) {
 			}
 			else {
 				$att_items += $_POST[$id.'-'.$ot];
-				write_attendance($id, $ot, $_POST[$id.'-'.$ot], $_POST['attend_date']);
+				write_attendance($id, $ot, $_POST[$id.'-'.$ot], $_POST['AttDate']);
 			}
         }
     }
