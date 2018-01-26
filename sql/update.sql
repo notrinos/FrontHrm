f3519f784412d1ad0475766642a136118fcb6ae7
Phương <trananhphuong83@gmail.com>
2018-01-26 18:45:42 +0700

    Restructuring

--- sql/update.sql
+++ sql/update.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `0_employee` (
     `emp_id` int(11) NOT NULL AUTO_INCREMENT,
     `emp_first_name` varchar(100) DEFAULT NULL,
     `emp_last_name` varchar(100) DEFAULT NULL,
-	`gender` tinyint(1) NOT NULL DEFAULT 0,
+	  `gender` tinyint(1) NOT NULL DEFAULT 0,
     `emp_address` tinytext,
     `emp_mobile` varchar(30) DEFAULT NULL,
     `emp_email` varchar(100) DEFAULT NULL,
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `0_salaryscale` (
     `scale_id` int(11) NOT NULL AUTO_INCREMENT,
     `scale_name` text NOT NULL,
     `inactive` tinyint(1) NOT NULL DEFAULT 0,
+    `pay_basis` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = monthly, 1 = daily',
     PRIMARY KEY (`scale_id`)
 ) ENGINE=InnoDB;
 
@@ -52,7 +53,8 @@ DROP TABLE IF EXISTS `0_attendance`;
 CREATE TABLE IF NOT EXISTS `0_attendance` (
     `emp_id` int(11) NOT NULL,
     `overtime_id` int(11) NOT NULL,
-    `hours_no` int(11) NOT NULL DEFAULT 0,
+    `hours_no` float(5) NOT NULL DEFAULT 0,
+    `rate` float(5) NOT NULL DEFAULT 1,
     `att_date` date NOT NULL,
     PRIMARY KEY (`emp_id`,`overtime_id`,`att_date`)
 ) ENGINE=InnoDB;
@@ -82,25 +84,44 @@ CREATE TABLE IF NOT EXISTS `0_salary_structure` (
   `pay_rule_id` int(11) NOT NULL,
   `pay_amount` double NOT NULL,
   `type` tinyint(1) NOT NULL COMMENT '0 for credit, 1 for debit',
+  `is_basic` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 
-DROP TABLE IF EXISTS `0_payslip_detail`;
-CREATE TABLE IF NOT EXISTS `0_payslip_detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `payslip_no` int(11) NOT NULL,
+DROP TABLE IF EXISTS `0_payslip`;
+CREATE TABLE IF NOT EXISTS `0_payslip` (
+  `payslip_no` int(11) NOT NULL AUTO_INCREMENT,
+  `trans_no` int(11) NOT NULL DEFAULT 0,
+  `emp_id` int(11) NOT NULL,
   `generated_date` date NOT NULL,
-  `to_the_order_of` varchar(255) NOT NULL,
   `from_date` date NOT NULL,
   `to_date` date NOT NULL,
   `leaves` int(11) NOT NULL,
   `deductable_leaves` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `payable_amount` double NOT NULL DEFAULT 0,
+  `salary_amount` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`payslip_no`)
 ) ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `0_payslip_details`;
+CREATE TABLE IF NOT EXISTS `0_payslip_details` (
+  `payslip_no` int(11) NOT NULL AUTO_INCREMENT,
+  `detail` int(11) NOT NULL DEFAULT 0,
+  `amount` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`payslip_no`, `detail`)
+) ENGINE=InnoDB;
 
-ALTER TABLE `0_gl_trans` ADD `payslip_no` INT(11) NOT NULL DEFAULT 0;
+DROP TABLE IF EXISTS `0_employee_trans`;
+CREATE TABLE IF NOT EXISTS `0_employee_trans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `trans_no` int(11) NOT NULL DEFAULT 0,
+  `payslip_no` int(11) NOT NULL,
+  `pay_date` date NOT NULL,
+  `to_the_order_of` varchar(255) NOT NULL,
+  `pay_amount` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
 DELETE FROM `0_sys_prefs` WHERE `0_sys_prefs`.`name` = 'payroll_deductleave_act';
 DELETE FROM `0_sys_prefs` WHERE `0_sys_prefs`.`name` = 'payroll_month_work_days';
