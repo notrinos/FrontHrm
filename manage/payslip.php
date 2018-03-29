954f95f013d223229a6203f39fb19690a6ca8bdc
Phương <trananhphuong83@gmail.com>
2018-03-29 08:13:18 +0700

    Update payslip.php

--- manage/payslip.php
+++ manage/payslip.php
@@ -3,7 +3,7 @@
 |                        FrontHrm                        |
 |--------------------------------------------------------|
 |   Creator: Phương                                      |
-|   Date :   09-Jul-2017                                  |
+|   Date :   09-Jul-2017                                 |
 |   Description: Frontaccounting Payroll & Hrm Module    |
 |   Free software under GNU GPL                          |
 |                                                        |
@@ -28,10 +28,10 @@ include_once($path_to_root . "/modules/FrontHrm/includes/frontHrm_db.inc");
 include_once($path_to_root . "/modules/FrontHrm/includes/frontHrm_ui.inc");
 
 if (isset($_GET['ModifyPaySlip'])) {
-	$_SESSION['page_title'] = sprintf(_("Modifying Employee Payslip # %d."), 
-		$_GET['trans_no']);
+	$_SESSION['page_title'] = sprintf(_("Modifying Employee Payslip # %d."), $_GET['trans_no']);
 	$help_context = "Modifying Employee Payslip";
-} else
+}
+else
 	$_SESSION['page_title'] = _($help_context = "Employee Payslip Entry");
 
 page($_SESSION['page_title'], false, false, "", $js);
@@ -53,17 +53,17 @@ if (isset($_GET['AddedID'])) {
     $payslip_no = get_payslip($trans_no)['payslip_no'];
 
     if($payslip_no) {
-    	display_notification_centered( _("Payslip #$payslip_no has been entered"));
+    	display_notification_centered( sprintf(_("Payslip #%d has been entered"), $payslip_no));
         display_note(get_gl_view_str($trans_type, $trans_no, _("&View this Transaction")));
 
 	    reset_focus();
-	    hyperlink_params($path_to_root."/modules/FrontHrm/manage/pay_advice.php", _("Make Payment &Advice for this Payslip"), "PayslipNo=$payslip_no");
-	    hyperlink_params($_SERVER['PHP_SELF'], _("Enter &New Payslip"), "NewPayslip=Yes");
+	    hyperlink_params($path_to_root.'/modules/FrontHrm/manage/pay_advice.php', _('Make Payment &Advice for this Payslip'), 'PayslipNo='.$payslip_no);
+	    hyperlink_params($_SERVER['PHP_SELF'], _('Enter &New Payslip'), 'NewPayslip=Yes');
 
-	    hyperlink_params("$path_to_root/admin/attachments.php", _("Add an Attachment"), "filterType=$trans_type&trans_no=$trans_no");
+	    hyperlink_params($path_to_root.'/admin/attachments.php', _('Add an Attachment'), 'filterType='.$trans_type.'&trans_no='.$trans_no);
     }
    	else
-   		display_error(_("Payslip number does not exist"));
+   		display_error(_('Payslip number does not exist'));
    	
 	display_footer_exit();
 } 
@@ -72,10 +72,10 @@ elseif (isset($_GET['UpdatedID'])) {
 	$trans_no = $_GET['UpdatedID'];
 	$trans_type = ST_JOURNAL;
 
-   	display_notification_centered( _("Employee Payslip has been updated") . " #$trans_no");
-    display_note(get_gl_view_str($trans_type, $trans_no, _("&View this Journal Entry")));
+   	display_notification_centered( _('Employee Payslip has been updated #') . $trans_no);
+    display_note(get_gl_view_str($trans_type, $trans_no, _('&View this Journal Entry')));
 
-   	hyperlink_no_params($path_to_root."/gl/inquiry/payslip_inquiry.php", _("Return to Payslip &Inquiry"));
+   	hyperlink_no_params($path_to_root.'/gl/inquiry/payslip_inquiry.php', _('Return to Payslip &Inquiry'));
 
 	display_footer_exit();
 }
@@ -89,8 +89,8 @@ elseif (isset($_GET['ModifyPaySlip'])) {
     check_is_editable($_GET['trans_type'], $_GET['trans_no']);
     
 	if (!isset($_GET['trans_type']) || $_GET['trans_type']!= 0) {
-		display_error(_("You can edit directly only journal entries created via Journal Entry page."));
-		hyperlink_params("$path_to_root/gl/gl_journal.php", _("Entry &New Journal Entry"), "NewJournal=Yes");
+		display_error(_('You can edit directly only journal entries created via Journal Entry page.'));
+		hyperlink_params($path_to_root.'/gl/gl_journal.php', _('Entry &New Journal Entry'), 'NewJournal=Yes');
 		display_footer_exit();
 	}
 	create_cart($_GET['trans_type'], $_GET['trans_no']);
@@ -150,58 +150,58 @@ function create_cart($type=0, $trans_no=0) {
 function validate_payslip_generation() {
 
 	if (!$_POST['person_id']) {
-		display_error(_("Employee not selected"));
+		display_error(_('Employee not selected'));
 		set_focus('person_id');
 		return false;
 	} 
 	if (!is_date($_POST['from_date'])) {
-		display_error(_("The entered date is invalid."));
+		display_error(_('The entered date is invalid.'));
 		set_focus('from_date');
 		return false;
 	}
 	if (!is_date($_POST['to_date'])) {
-		display_error(_("The entered date is invalid."));
+		display_error(_('The entered date is invalid.'));
 		set_focus('to_date');
 		return false;
 	}
 	if(payslip_generated_for_date($_POST['from_date'], $_POST['person_id'])) {
-        display_error("Selected date has already paid for this person");
+        display_error('Selected date has already paid for this person');
         set_focus('from_date');
         return false;
     }
     if(payslip_generated_for_date($_POST['to_date'], $_POST['person_id'])) {
-        display_error("Selected date has already paid for this person");
+        display_error('Selected date has already paid for this person');
         set_focus('to_date');
         return false;
     }
     if(payslip_generated_for_period($_POST['from_date'], $_POST['to_date'], $_POST['person_id'])) {
-    	display_error("Selected period contains a period that has already been paid for this person");
+    	display_error('Selected period contains a period that has already been paid for this person');
         set_focus('from_date');
         return false;
     }
     if(date_comp($_POST['from_date'], $_POST['to_date']) > 0) {
-        display_error("End date cannot be before the start date");
+        display_error('End date cannot be before the start date');
         set_focus('from_date');
         return false;
     }
     if (date_comp($_POST['from_date'], Today()) > 0) {
-		display_error(_("Cannot pay for the date in the future."));
+		display_error(_('Cannot pay for the date in the future.'));
 		set_focus('from_date');
 		return false;
 	}
 	if (date_comp($_POST['to_date'], Today()) > 0) {
-		display_error(_("Cannot pay for the date in the future."));
+		display_error(_('Cannot pay for the date in the future.'));
 		set_focus('to_date');
 		return false;
 	}
     if(!check_employee_hired($_POST['person_id'], $_POST['from_date'])) {
-        display_error("Cannot pay before hired date");
+        display_error('Cannot pay for the date before hired date');
         set_focus('from_date');
         return false;
     }
     // The following two cases need to be set in correct order
     if(!employee_has_salary_scale($_POST['person_id'])) {
-    	display_error("Selected Employee does not have a Salary Scale, please define it first.");
+    	display_error('Selected Employee does not have a Salary Scale, please define it first.');
     	set_focus('person_id');
     	return false;
     }
@@ -224,22 +224,22 @@ if (isset($_POST['Process'])) {
 	$input_error = 0;
 
 	if ($_SESSION['journal_items']->count_gl_items() < 1) {
-		display_error(_("You must enter at least one journal line."));
+		display_error(_('You must enter at least one journal line.'));
 		set_focus('code_id');
 		$input_error = 1;
 	}
 	if (abs($_SESSION['journal_items']->gl_items_total()) > 0.0001) {
-		display_error(_("The journal must balance (debits equal to credits) before it can be processed."));
+		display_error(_('The journal must balance (debits equal to credits) before it can be processed.'));
 		set_focus('code_id');
 		$input_error = 1;
 	}
 	if (!is_date($_POST['date_'])) {
-		display_error(_("The entered date is invalid."));
+		display_error(_('The entered date is invalid.'));
 		set_focus('date_');
 		$input_error = 1;
 	} 
 	elseif (!is_date_in_fiscalyear($_POST['date_'])) {
-		display_error(_("The entered date is out of fiscal year or is closed for further data entry."));
+		display_error(_('The entered date is out of fiscal year or is closed for further data entry.'));
 		set_focus('date_');
 		$input_error = 1;
 	} 
@@ -283,9 +283,9 @@ if (isset($_POST['Process'])) {
 	new_doc_date($_POST['date_']);
 	unset($_SESSION['journal_items']);
 	if($new)
-		meta_forward($_SERVER['PHP_SELF'], "AddedID=$trans_no");
+		meta_forward($_SERVER['PHP_SELF'], 'AddedID='.$trans_no);
 	else
-		meta_forward($_SERVER['PHP_SELF'], "UpdatedID=$trans_no");
+		meta_forward($_SERVER['PHP_SELF'], 'UpdatedID='.$trans_no);
 }
 
 //--------------------------------------------------------------------------
@@ -300,37 +300,37 @@ if (isset($_POST['CancelOrder']) || !isset($_POST['person_id'])) {
 
 function check_item_data() {
 	if (isset($_POST['dimension_id']) && $_POST['dimension_id'] != 0 && dimension_is_closed($_POST['dimension_id'])) {
-		display_error(_("Dimension is closed."));
+		display_error(_('Dimension is closed.'));
 		set_focus('dimension_id');
 		return false;
 	}
 	if (isset($_POST['dimension2_id']) && $_POST['dimension2_id'] != 0 && dimension_is_closed($_POST['dimension2_id'])) {
-		display_error(_("Dimension is closed."));
+		display_error(_('Dimension is closed.'));
 		set_focus('dimension2_id');
 		return false;
 	}
 	if (!(input_num('AmountDebit')!=0 ^ input_num('AmountCredit')!=0) ) {
-		display_error(_("You must enter either a debit amount or a credit amount."));
+		display_error(_('You must enter either a debit amount or a credit amount.'));
 		set_focus('AmountDebit');
     		return false;
   	}
 	if (strlen($_POST['AmountDebit']) && !check_num('AmountDebit', 0)) {
-        display_error(_("The debit amount entered is not a valid number or is less than zero."));
+        display_error(_('The debit amount entered is not a valid number or is less than zero.'));
 		set_focus('AmountDebit');
         return false;
   	}
     elseif (strlen($_POST['AmountCredit']) && !check_num('AmountCredit', 0)) {
-        display_error(_("The credit amount entered is not a valid number or is less than zero."));
+        display_error(_('The credit amount entered is not a valid number or is less than zero.'));
 		set_focus('AmountCredit');
         return false;
   	}
 	if (!is_tax_gl_unique(get_post('code_id'))) {
-   		display_error(_("Cannot post to GL account used by more than one tax type."));
+   		display_error(_('Cannot post to GL account used by more than one tax type.'));
 		set_focus('code_id');
    		return false;
 	}
-	if (!$_SESSION["wa_current_user"]->can_access('SA_BANKJOURNAL') && is_bank_account($_POST['code_id'])) {
-		display_error(_("You cannot make a journal entry for a bank account. Please use one of the banking functions for bank transactions."));
+	if (!$_SESSION['wa_current_user']->can_access('SA_BANKJOURNAL') && is_bank_account($_POST['code_id'])) {
+		display_error(_('Cannot make a journal entry for a bank account. Use banking functions for bank transactions.'));
 		set_focus('code_id');
 		return false;
 	}
@@ -347,8 +347,7 @@ function handle_update_item() {
     	else
     		$amount = -input_num('AmountCredit');
 
-    	$_SESSION['journal_items']->update_gl_item($_POST['Index'], $_POST['code_id'], 
-    	    $_POST['dimension_id'], $_POST['dimension2_id'], $amount, $_POST['LineMemo'], '', get_post('person_id'));
+    	$_SESSION['journal_items']->update_gl_item($_POST['Index'], $_POST['code_id'], $_POST['dimension_id'], $_POST['dimension2_id'], $amount, $_POST['LineMemo'], '', get_post('person_id'));
     }
 	line_start_focus();
 }
@@ -367,8 +366,7 @@ function handle_new_item() {
 	else
 		$amount = -input_num('AmountCredit');
 	
-	$_SESSION['journal_items']->add_gl_item($_POST['code_id'], $_POST['dimension_id'],
-		$_POST['dimension2_id'], $amount, $_POST['LineMemo'], '', get_post('person_id'));
+	$_SESSION['journal_items']->add_gl_item($_POST['code_id'], $_POST['dimension_id'], $_POST['dimension2_id'], $amount, $_POST['LineMemo'], '', get_post('person_id'));
 	line_start_focus();
 }
 
@@ -406,20 +404,19 @@ if (!count($_SESSION['journal_items']->gl_items)) {
 }
 
 div_start('payslip_trans');
-	if (count($_SESSION['journal_items']->gl_items)) {
-		start_table(TABLESTYLE_NOBORDER, "width='90%'", 10);
-		start_row();
-		echo "<td>";
-		display_gl_items(_("Rows"), $_SESSION['journal_items']);
-		gl_options_controls();
-		echo "</td>";
-		end_row();
-		end_table(1);
+if (count($_SESSION['journal_items']->gl_items)) {
+	start_table(TABLESTYLE_NOBORDER, "width='90%'", 10);
+	start_row();
+	echo "<td>";
+	display_gl_items(_("Rows"), $_SESSION['journal_items']);
+	gl_options_controls();
+	echo "</td>";
+	end_row();
+	end_table(1);
 
-		submit_center_first('Process', _("Process PaySlip"), _('Process journal entry only if debits equal to credits'));
-
-		submit_center_last('CancelOrder', _("Cancel"), _('Cancels document entry or removes Gl items'), true);
-	}
+	submit_center_first('Process', _('Process PaySlip'), _('Process journal entry only if debits equal to credits'));
+	submit_center_last('CancelOrder', _('Cancel'), _('Cancels document entry or removes Gl items'), true);
+}
 div_end();
 
 // highlight_string("<?php\n" . var_export($_SESSION['journal_items'], true));
